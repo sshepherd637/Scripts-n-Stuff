590b5f223dc6ce15c09695f9dc4b6976ce123a37
Sam Shepherd <sshepherd04@qub.ac.uk>
2021-07-18 19:11:55 +0100

    added misc directory

---
+++ AiiDA-tools/Misc-scripts/nodes_to_labels.py
@@ -0,0 +1,23 @@
+#!/Users/samshepherd/envs/AiiDA_env/bin/python3
+
+# A quick script to convert a file with the pks of a group of nodes ( as output by verdi process list ) to a file with their labels.
+
+import os
+import aiida
+
+# specify the node file path
+node_file = os.path.join(os.getcwd(), 'nodes')
+
+# open it for the pks
+with open(node_file, 'r') as handle:
+	data = handle.readlines()
+
+# open the label file to write to
+new_file = open('labels', 'w+')
+
+# iterate over all lines
+for i in range(len(data)):
+	pk = data[i].split()[0]
+	node = load_node(pk)
+	label = node.label
+	new_file.writelines(f'{label}\n')
---
+++ AiiDA-tools/Misc-scripts/readme.md
@@ -0,0 +1,3 @@
+# Miscellanious
+
+This directory holds other scripts I have found useful when operating with AiiDA via the command line.
